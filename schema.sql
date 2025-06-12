a1a13d66244ab50d9c3d22f682533af3550d9edb
Kumeuchi <55736659+kmkm10@users.noreply.github.com>
2025-06-12 21:24:14 +0900

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -2,13 +2,18 @@ DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS players;
 
 CREATE TABLE users (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    id {auto_increment_syntax},
     username TEXT UNIQUE NOT NULL,
-    password TEXT NOT NULL
+    password TEXT NOT NULL,
+    
+    -- ▼▼▼【ここから追記】▼▼▼
+    reset_token TEXT,
+    reset_token_expiry REAL
+    -- ▲▲▲【ここまで追記】▲▲▲
 );
 
 CREATE TABLE players (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    id {auto_increment_syntax},
     user_id INTEGER NOT NULL,
     last_update_time REAL NOT NULL,
     research_points REAL NOT NULL,
@@ -24,10 +29,6 @@ CREATE TABLE players (
     genesis_shifts INTEGER NOT NULL DEFAULT 0,
     perm_bonus_rp_level INTEGER NOT NULL DEFAULT 0,
     perm_bonus_money_level INTEGER NOT NULL DEFAULT 0,
-
-    -- ▼▼▼【ここを追記】▼▼▼
-    run_start_time REAL NOT NULL DEFAULT 0 -- 現在の周回の開始時刻
-    -- ▲▲▲【ここまで追記】▲▲▲
-    ,
+    run_start_time REAL NOT NULL DEFAULT 0,
     FOREIGN KEY (user_id) REFERENCES users (id)
 );
